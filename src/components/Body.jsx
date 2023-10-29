2ef56f950e5c243acf9b532bab047ecd2e6d7df9
nayan <nayannjddas@gmail.com>
2023-10-30 04:40:01 +0530

    React hook- useState

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,19 @@
 import ResCard from "./ResCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 const Body = () => {
+  const [res, setRes] = useState(resList);
   return (
     <div className="body">
+      <button
+        onClick={() => {
+          setRes(res.filter((x) => x.info.avgRating > 4.3));
+        }}
+      >
+        Top Rated
+      </button>
       <div className="resContainer">
-        {resList.map((item) => (
+        {res.map((item) => (
           <ResCard key={item.info.id} cardData={item} />
         ))}
       </div>
