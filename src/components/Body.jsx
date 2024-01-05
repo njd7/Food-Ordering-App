d1850212e1206cd7975bd5e8c25df09bc067ab95
nayan <nayannjddas@gmail.com>
2024-01-05 18:07:47 +0530

    updated favicon

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -21,7 +21,7 @@ const Body = (props) => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.9124336&lng=75.7872709&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=27.4705449&lng=94.91246249999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
 
     // optional chaining
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const LOGO_URL = "https://1000logos.net/wp-content/uploads/2021/05/Swiggy-emblem.png";
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const MENU_URL =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.9124336&lng=75.7872709&restaurantId=";
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=27.4705449&lng=94.91246249999999&restaurantId=";
 export const VEG = "https://img.icons8.com/color/48/vegetarian-food-symbol.png";
 export const NON_VEG = "https://img.icons8.com/color/48/non-vegetarian-food-symbol.png";
